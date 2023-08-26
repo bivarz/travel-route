9763d9551694c0bea8d28cc3d4583fc975b421f6
bivarz <paulo.bivar@gmail.com>
2023-08-26 00:28:31 -0300

    feat: adding mouseEvent at the calendar component

--- travel-route-app/src/components/Calendar/calendar.component.tsx
+++ travel-route-app/src/components/Calendar/calendar.component.tsx
@@ -17,7 +17,7 @@ import { CustomInput } from "../InputField/index";
 import "react-calendar/dist/Calendar.css";
 import ArrowLeft from "../../assets/icons/arrow-left-icon.svg";
 import ArrowRight from "../../assets/icons/arrow-right-icon.svg";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 type ValuePiece = Date | null;
 
@@ -25,14 +25,38 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 export const CalendarCustom = () => {
   const [value, onChange] = useState<Value>(new Date());
+  const [showCalendar, setShowCalendar] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const calendarRef = useRef<HTMLDivElement>(null);
   const dateNow = new Date(Date.now()).toLocaleString().split(",")[0];
 
   const abbrDaysOfWeek = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
 
+  const handleDocumentClick = (event: MouseEvent) => {
+    if (
+      inputRef.current &&
+      inputRef.current !== (event.target as HTMLInputElement) &&
+      calendarRef.current &&
+      !calendarRef.current.contains(event.target as Node)
+    ) {
+      setShowCalendar(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleDocumentClick);
+    return () => {
+      document.removeEventListener("click", handleDocumentClick);
+    };
+  }, []);
+
   return (
     <Container>
       <Content>
-        <InputContainer>
+        <InputContainer
+          onMouseOver={() => setShowCalendar(true)}
+          ref={inputRef}
+        >
           <CustomInput
             fullWidth={false}
             label="Date"
@@ -40,34 +64,36 @@ export const CalendarCustom = () => {
             value={`${dateNow}`}
           />
         </InputContainer>
-        <CalendarContainer>
-          <FloatingBox>
-            <Navigation>
-              <NavigationButton>
-                <img src={ArrowLeft} alt="left" />
-              </NavigationButton>
-              <SelectDateArea>
-                <SelectMonth>JAN</SelectMonth>
-                <SelectYear>2023</SelectYear>
-              </SelectDateArea>
-              <NavigationButton>
-                <img src={ArrowRight} alt="right" />
-              </NavigationButton>
-            </Navigation>
-            <WeekDays>
-              <Days>
-                {abbrDaysOfWeek.map((day, index) => (
-                  <p key={index}>{day}</p>
-                ))}
-              </Days>
-              <StyledCalendar
-                className="calendar"
-                onChange={onChange}
-                value={value}
-              />
-            </WeekDays>
-          </FloatingBox>
-        </CalendarContainer>
+        {showCalendar && (
+          <CalendarContainer ref={calendarRef}>
+            <FloatingBox>
+              <Navigation>
+                <NavigationButton>
+                  <img src={ArrowLeft} alt="left" />
+                </NavigationButton>
+                <SelectDateArea>
+                  <SelectMonth>JAN</SelectMonth>
+                  <SelectYear>2023</SelectYear>
+                </SelectDateArea>
+                <NavigationButton>
+                  <img src={ArrowRight} alt="right" />
+                </NavigationButton>
+              </Navigation>
+              <WeekDays>
+                <Days>
+                  {abbrDaysOfWeek.map((day, index) => (
+                    <p key={index}>{day}</p>
+                  ))}
+                </Days>
+                <StyledCalendar
+                  className="calendar"
+                  onChange={onChange}
+                  value={value}
+                />
+              </WeekDays>
+            </FloatingBox>
+          </CalendarContainer>
+        )}
       </Content>
       {/* <ErrorMsg>Select passengers</ErrorMsg> */}
     </Container>
