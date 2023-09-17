404c0e2ced873c18caa4214a793b5cb47551d0d0
bivarz <paulo.bivar@gmail.com>
2023-09-16 23:01:50 -0300

    feat: adding total of distance in kms

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -6,3 +6,28 @@ export function formatDateToDDMMYYYY(dateString: Date) {
 
   return `${day}/${month}/${year}`;
 }
+
+export function formatDateMMDDYYYY(inputDate: string) {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const date = new Date(inputDate);
+
+  const month = months[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  return `${month} ${day}, ${year}`;
+}
